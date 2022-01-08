558ded936fe5f76c7c35d35f724df1591be790ac
J. Nick Koston <nick@koston.org>
2022-01-08 11:38:24 -1000

    Bump version: 0.1.1 → 0.2.0

--- discovery30303/__init__.py
+++ discovery30303/__init__.py
@@ -7,7 +7,7 @@ from typing import Callable, Dict, List, Optional, Tuple
 
 _LOGGER = logging.getLogger(__name__)
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
 MAX_UPDATES_WITHOUT_RESPONSE = 4
